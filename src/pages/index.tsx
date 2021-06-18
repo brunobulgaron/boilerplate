8f3ca0f9178de2b28ebc93255a1e13000d9defc8
brunobulgaron <brunohbulgaron@gmail.com>
2021-06-18 17:00:21 -0300

    Added initial config files and packages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head"
+import Image from "next/image"
+import styles from "../../styles/Home.module.css"
 
-export default function Home() {
+type HomeProps = {
+  title: string
+}
+
+export default function Home({ title = "React avançado" }: HomeProps) {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,12 +16,10 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>{title}</h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -58,7 +60,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
